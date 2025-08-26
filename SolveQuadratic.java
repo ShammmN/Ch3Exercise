01a1bf59640a3d0c640643b2ac047a182ccbd43d
Sham Nemer <snemer@my.waketech.edu>
2025-08-26 10:19:34 -0400

    adding code

---
+++ SolveQuadratic.java
@@ -0,0 +1,23 @@
+/*
+* Name: Ch3Code
+* Purpose: Get the roots by using a quadratic formula
+* Programmer: Sham Nemer
+* Date: 8 / 26 / 2025
+*/ 
+
+import java.util.Scanner;
+
+public class SolveQuadratic {
+    public static void main(String[] args) {
+        Scanner userValues = new Scanner(System.in);
+
+        System.out.println("Please enter values for A, B, and C:");
+
+        double aValue = userValues.nextDouble();
+        double bValue = userValues.nextDouble();
+        double cValue = userValues.nextDouble();        
+
+        double quadForm =  0 - bValue + ((bValue))    
+
+    }
+}
